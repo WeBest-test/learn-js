d160289538ea840465cf2dac932dff9216505bbd
lgh06 <hnnk@qq.com>
2018-03-06 03:40:40 +0000

    basic js

---
+++ base-js/string.mjs
@@ -0,0 +1,12 @@
+let str1 = new String("111");
+let str2 = new String("222");
+console.log(str1 == str2);
+console.log(typeof str1);
+
+let str3 = "111";
+let str4 = "111";
+
+console.log(str3 === str4);
+console.log(typeof str3)
+
+
