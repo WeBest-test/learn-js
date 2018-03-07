1f7545fffc8c87ca2ea24639f1822727de4c6436
lgh06 <hnnk@qq.com>
2018-03-07 04:24:35 +0000

    add comment

--- base-js/111.mjs
+++ base-js/111.mjs
@@ -1,13 +1,15 @@
 let p = console.log;
 
-p(typeof 111)
-p(typeof "111")
-p(typeof true)
-p(typeof null)   // object
+p(typeof true) //boolean
+p(typeof 111) // number
+p(typeof "111") //string
+p(typeof null)   // null是一种基本数据类型，但typeof运算结果为object
 p(typeof undefined) // undefined
+
 p(typeof {a:111})
-p(typeof []) // object
+p(typeof []) // object  数组是一种object
 p(typeof (()=>{})) // object
 
 // bool number string null undefined  symbol
 // object(array function)
+
