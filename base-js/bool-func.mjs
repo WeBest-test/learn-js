7de1129230697d249bd7c591af0365ad10ea8186
lgh06 <hnnk@qq.com>
2018-03-08 02:58:11 +0000

    boolean & equal & compare

--- base-js/bool-func.mjs
+++ base-js/bool-func.mjs
@@ -3,6 +3,7 @@ p(typeof true)
 
 p( true && (()=>console.log(666))() )
 
+
 if(true && function(){}){
   p("1111")
 }
@@ -12,3 +13,20 @@ p(true && function(){})
 p(11 && 22 && 33) // 33
 p(function(){console.log(222)}) // 是一个函数
 p(function(){console.log(999)}()) // 是一个函数执行后的返回值 undefined
+
+p("==========")
+p(Number(null) === 0 )
+
+p("================")
+p(Boolean(""))  
+if(""){ // 其实为Boolean函数转换
+  p("1")
+}
+
+p("=======")
+p(0 == "")   // 相等的转换和Boolean转换规则不同。
+p(0 == "0") // 字符串与数值判断相等，先把字符串转换为数值
+
+p("===")
+p(false == "0") // 布尔值与字符串判断相等，各自转换为数值后比较。
+p(Boolean("0")) // true
